b7331c3424a3f8ad6dbb4a049d21b5bcbb576556
adamprinciotta <adamprinciotta@gmail.com>
2023-01-17 17:05:40 -0500

    Small updates to the CSS. More boilerplate assistance code for it

--- ToDo/src/Task.jsx
+++ ToDo/src/Task.jsx
@@ -18,20 +18,23 @@ function Task(props) {
             // Must return the jsx that I want to render
           return (
             //The whole task
-            <><div key={index} className="task">
-                {/* Checkbox input */}
-                <input 
-                type="checkbox"
-                style={{ height: "30px", width: "30px"}}
-                checked={item.checked}
-                onChange={() => handleChecked(index)}
-                /> 
-                <div className="taskName" style={{
-                    textDecoration: item.checked ? "line-through" : "none", paddingLeft: "20px" }}>{item.name}
-                </div>
-                <div className="sectionName" style={{position: 'relative', right: '-50px'}}>
-                  {item.section}
-                </div>
+            <>
+            <div className="taskContainer">
+                <div key={index} className="task">
+                  {/* Checkbox input */}
+                  <input 
+                  className="checkboxStyle"
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={() => handleChecked(index)}
+                  /> 
+                  <div className="taskName" style={{
+                      textDecoration: item.checked ? "line-through" : "none", paddingLeft: "20px" }}>{item.name}
+                  </div>
+              </div>
+              <div className="sectionName" style={{position: 'relative', right: '-50px'}}>
+                    {item.section}
+              </div>
             </div>
             <div className='spacer' style={{paddingTop: "15px"}}></div>
             </>
